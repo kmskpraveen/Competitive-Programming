9c210f4d8445853ac69359a2e07d50abd6a7dfed
kmskpraveen <31201098+kmskpraveen@users.noreply.github.com>
2018-07-08 10:02:54 +0530

    Add files via upload

---
+++ Week-2/Day-6/Programs/InplaceShuffle.py
@@ -0,0 +1,19 @@
+import random
+
+def shuffle(the_list):
+
+	# Shuffle the input in place
+	if(len(the_list)>1):
+		for i in range(len(the_list)):
+			j = random.randint(i,len(the_list)-1)
+			if i!=j:
+				the_list[i],the_list[j] = the_list[j],the_list[i]
+
+
+
+sample_list = [1, 2, 3, 4, 5]
+print 'Sample list:', sample_list
+
+print 'Shuffling sample list...'
+shuffle(sample_list)
+print sample_list
